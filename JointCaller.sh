006e99a8971159eba72b8d0b664785692ecdc3dd
ignatiusm <ignatius.menzies@gmail.com>
2024-10-05 10:08:51 +1300

    Update config file and location

--- JointCaller.sh
+++ JointCaller.sh
@@ -21,4 +21,4 @@ echo "Move to correct directory ..."
 cd $INSTANCE_PARENT/$REPO
 
 echo "Run pipeline ..."
-nextflow run JointCaller.nf -c JointCaller.hg38.local.config --sample_name_map sample_name_map --callset_name jointCallRef38 -profile gadi,gatk4 -resume
+nextflow run JointCaller.nf -c nci_configs/JointCaller.hg38.local.config --sample_name_map sample_inputs/sample_name_map --callset_name jointCallRef38 -profile gadi,gatk4 -resume
